17144579bbfa36847715dc0ced0a15f403b0b842
MonaR98 <mona.r.sharma98@gmail.com>
2024-01-13 12:57:23 +0530

    initial commit

---
+++ src/contants/icons.js
@@ -0,0 +1,10 @@
+const Back = require('../assets/icons/back.png');
+const Bag = require('../assets/icons/bag.png');
+const Category = require('../assets/icons/category.png');
+const Heart = require('../assets/icons/heart.png');
+const Home = require('../assets/icons/home.png');
+const ImageIcon = require('../assets/icons/image_icon.png');
+const More = require('../assets/icons/more.png');
+const Search = require('../assets/icons/search.png');
+
+export default { Back, Bag, Category, Heart, Home, ImageIcon, More, Search}
---
+++ src/contants/theme.js
@@ -0,0 +1,10 @@
+const COLORS = {
+    primary_blue: '#153075',
+    secondary_blue: '#2A4BA0',
+    primary_yellow: '#F9B023',
+    secondary_yellow: '#FFC83A',
+    
+    light_gray: '#F8F9FB',
+}
+
+export {COLORS}
